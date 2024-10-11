beb229137b35b948472d968bc52fbd0b3fbbccd7
Hellod035 <hellod035@163.com>
2024-10-11 14:04:51 +0800

    简化配置文件

--- run.sh
+++ run.sh
@@ -4,39 +4,31 @@ cd /opt/data/private/SWD_ROOT/human2humanoid/legged_gym
 
 # OmniH2O Training and Playing Teacher Policy 
 python legged_gym/scripts/train_hydra.py \
---config-name=config_teleop \
-task=h1:teleop \
-run_name=OmniH2O_TEACHER \
-env.num_observations=913 \
-env.num_privileged_obs=990 \
-motion.teleop_obs_version=v-teleop-extend-max-full \
-motion=motion_full \
-motion.extend_head=True \
-num_envs=8192 \
-asset.zero_out_far=False \
-asset.termination_scales.max_ref_motion_distance=1.5 \
-sim_device=cuda:0 \
-motion.motion_file=resources/motions/h1/amass_phc_filtered.pkl \
-rewards=rewards_teleop_omnih2o_teacher \
-rewards.penalty_curriculum=True \
-rewards.penalty_scale=0.5
+--config-name=config_teleop_teacher
 
 # OmniH2O Play Teacher Policy
 python  legged_gym/scripts/play_hydra.py \
---config-name=config_teleop \
-task=h1:teleop \
-env.num_observations=913 \
-env.num_privileged_obs=990 \
-motion.future_tracks=True \
-motion.teleop_obs_version=v-teleop-extend-max-full \
-motion=motion_full  \
-motion.extend_head=True \
-asset.zero_out_far=False \
-asset.termination_scales.max_ref_motion_distance=10.0  \
-sim_device=cuda:0 \
-motion.motion_file=resources/motions/h1/amass_phc_filtered.pkl \
-rewards=rewards_teleop_omnih2o_teacher \
-load_run=24_10_11_01-28-50_OmniH2O_TEACHER \
-checkpoint=14500 \
+--config-name=config_teleop_teacher \
 num_envs=1 \
+motion.future_tracks=True \
+asset.termination_scales.max_ref_motion_distance=10.0  \
+load_run=24_10_11_01-28-50_OmniH2O_TEACHER \
+checkpoint=17500 \
 headless=False
+
+
+
+# OmniH2O Distill Student Policy
+python legged_gym/scripts/train_hydra.py \
+--config-name=config_teleop_student \
+train.dagger.load_run_dagger=24_10_11_01-28-50_OmniH2O_TEACHER \
+train.dagger.checkpoint_dagger=17500
+
+# OmniH2O Play Student Policy
+python legged_gym/scripts/play_hydra.py \
+--config-name=config_teleop_student \
+num_envs=1 \
+asset.termination_scales.max_ref_motion_distance=10.0 \
+load_run=OmniH2O_STUDENT \
+checkpoint=XXXX \
+headless=False 
