072d97b5804305fe7150af1cf5e6656c7e069f93
Hellod035 <hellod035@163.com>
2024-10-11 21:17:57 +0800

    1

--- run.sh
+++ run.sh
@@ -21,14 +21,16 @@ headless=False
 # OmniH2O Distill Student Policy
 python legged_gym/scripts/train_hydra.py \
 --config-name=config_teleop_student \
-train.dagger.load_run_dagger=24_10_11_01-28-50_OmniH2O_TEACHER \
-train.dagger.checkpoint_dagger=23000
+train.dagger.load_run_dagger=24_10_11_20-54-47_OmniH2O_TEACHER \
+train.dagger.checkpoint_dagger=0
 
 # OmniH2O Play Student Policy
 python legged_gym/scripts/play_hydra.py \
 --config-name=config_teleop_student \
 num_envs=1 \
+train.dagger.load_run_dagger=24_10_11_20-54-47_OmniH2O_TEACHER \
+train.dagger.checkpoint_dagger=0 \
 asset.termination_scales.max_ref_motion_distance=10.0 \
-load_run=OmniH2O_STUDENT \
-checkpoint=XXXX \
+load_run=24_10_11_20-56-57_OmniH2O_STUDENT \
+checkpoint=0 \
 headless=False 
